642a53fa0545c25a60d13035f33cef131a5bf65b
kingofclubstroy <jordanchew4@hotmail.com>
2022-07-09 12:49:56 -0400

    complete

--- contracts/KnowledgeTest.sol
+++ contracts/KnowledgeTest.sol
@@ -6,8 +6,41 @@ contract KnowledgeTest {
     string[] public tokens = ["BTC", "ETH"];
     address[] public players;
 
-    function changeTokens() public view {
-        string[] memory t = tokens;
-        t[0] = "VET";
+    address public owner;
+
+    constructor() {
+        owner = msg.sender;
+    }
+
+    function changeTokens() public {
+        tokens[0] = "VET";
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function transferAll(address payable to) public {
+
+        require(msg.sender == owner, "ONLY_OWNER");
+
+        (bool result, bytes memory data) = to.call{value: address(this).balance}("");
+
+        require(result, "Failure transfering");
+
+    }
+
+    function concatenate(string memory str1, string memory str2) external pure returns(string memory) {
+
+        return string(abi.encodePacked(str1, str2));
+
+    }
+
+    function start() external {
+        players.push(msg.sender);
+    }
+
+    receive() external payable {
+
     }
 }
